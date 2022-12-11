0385432d93a490a058c2c6158b32d39e4ac9dff9
JDL <josh.lilley98@gmail.com>
2022-12-11 17:58:25 +0000

    ELO

---
+++ ELO.py
@@ -0,0 +1,29 @@
+from ELO_Rank import rank
+
+##Elo rank system 
+#create database/ dictionary of teams & elo
+
+def main():
+#call a function that creates a team profile {name, ELO}
+    for teamA in df:
+        if (teamA in t for t in teams):
+            pass
+        else:
+            teamAdd(df[team])
+    for teamB in df:
+        if (teamB in t for t in teams)         
+           pass
+        else:
+            teamAdd(df[team])
+#call a function that takes game data & returns a new rank {name A, name B, score A, score B}
+
+#append the current team profile with new ELO
+
+import csv
+file = open('elo.csv')
+df = csv.reader(file)
+
+
+
+if __name__ == '__main__':
+    main()
