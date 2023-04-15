a3c9ca079fd3e0611977c1aafe146bfba5c86794
JDL <josh.lilley98@gmail.com>
2023-04-15 18:33:13 +0100

    Multi-Cancer Kidney data upload

--- UoE_Test.py
+++ UoE_Test.py
@@ -1,1 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+from sklearn.metrics import confusion_matrix
+import cv2
+import random
+import os
+from PIL import Image
+
 import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+
+dataset_dir = '/Multi Cancer'
